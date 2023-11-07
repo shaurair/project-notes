836cf493c0b8f17ea82ea1aa45c4b95d1436abb1
shaurair <shaurair@gmail.com>
2023-11-07 17:10:34 +0800

    Add: resize method in /member/update/image

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,33 +1,50 @@
 const token = require('../utilities/token');
 const multer = require('multer');
+const sharp = require('sharp');
 const imageStorage = multer.memoryStorage();
 const upload = multer({storage: imageStorage});
 
-const updateImage = (req, res) => {
-	// debug temp
-	console.log(req.file)
+const updateImage = async (req, res) => {
+	let memberInfo;
 	let existFilename = null;
 
 	try {
 		let userToken = req.headers.authorization.replace('Bearer ', '');
-		existFilename = token.decode(userToken).file_name;
+		memberInfo = token.decode(userToken);
+		existFilename = memberInfo.file_name;
 	}
 	catch(err) {
 		res.status(403).send({data: {"message" : "User not log in"}});
 	}
 
 	if(req.file) {
-		// resize
+		const fileMimeType = req.file.mimetype;
 
-		if(existFilename == null) {
-			// Add
+		if(fileMimeType === 'image/jpeg' || fileMimeType === 'image/png') {
+			// resize
+			let imageResize = req.file.buffer;
+			const image = sharp(imageResize);
+			const imageMetaData = await image.metadata();
+
+			if(imageMetaData.height > 150) {
+				imageResize = await image.resize({height: 200, fit: 'contain'}).toBuffer();
+			}
+
+			if(existFilename == null) {
+				// Add
+				existFilename = memberInfo.id + '.' + imageMetaData.format;
+
+			}
+			else {
+				// Update
+			}
+
+			// debug temp
+			res.status(200).send({data: {"message" : "Well received"}});
 		}
 		else {
-			// Update
+			res.status(400).json({"message": "Upload file is not valid(only .png / .jpeg is allowed)"});
 		}
-
-		// debug temp
-		res.status(200).send({data: {"message" : "Well received"}});
 	}
 	else {
 		res.status(400).send({data: {"message" : "None file request"}});
