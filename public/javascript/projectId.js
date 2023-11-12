30a9d0f96221d4c4eb5c50665b8043ee28bd1525
shaurair <shaurair@gmail.com>
2023-11-13 00:28:28 +0800

    Add: edit project initial value

--- public/javascript/projectId.js
+++ public/javascript/projectId.js
@@ -5,6 +5,8 @@ const cancelEditBtn = document.getElementById('cancel-edit-project');
 const darkBackgrountElement = document.querySelector('.dark-background');
 const editProjectAreaElement = document.querySelector('.edit-project-area');
 let projectId = location.href.match(/\/project\/(\d+)/)[1];
+let projectData;
+let associate = {owner:{}, reviewer:{}, team:{}};
 
 async function initProjectId() {
 	await getUser();
@@ -26,11 +28,123 @@ async function getProjectContent() {
 	let result = await response.json();
 
 	if(response.ok) {
+		projectData = result;
+		projectData['owner'].forEach(userData => {
+			associate.owner[userData['id']] = true;
+		});
+		projectData['reviewer'].forEach(userData => {
+			associate.reviewer[userData['id']] = true;
+		});
+		projectData['team'].forEach(userData => {
+			associate.team[userData['id']] = true;
+		});
 		setProjectContent(result);
 	}
 	else {
 		alert('something went wrong, please redirect and try again');
 	}
+}
+
+function addClickEffect(imgUrl, userName, id, peopleListElement, associateRole) {
+		let elementContainer = document.createElement('div');
+		elementContainer.className = 'people-container';
+		peopleListElement.appendChild(elementContainer);
+
+		if(associateRole != 'team') {
+			let element = document.createElement('div');
+			element.className = 'people-img';
+			elementContainer.appendChild(element);
+			if(imgUrl != null) {
+				element.style.backgroundImage = `url(${imgUrl})`;
+			}
+		}
+
+		element = document.createElement('div');
+		element.className = 'people-text';
+		element.textContent = userName;
+		elementContainer.appendChild(element);
+
+		let closeElement = document.createElement('div');
+		closeElement.className = 'close mouseover';
+		closeElement.addEventListener('click', () => {
+			delete associate[associateRole][id];
+			peopleListElement.removeChild(elementContainer);
+		})
+
+		let closeIconElement = document.createElement('div');
+		closeIconElement.className = 'close-icon';
+
+		closeElement.appendChild(closeIconElement);
+		elementContainer.appendChild(closeElement);
+
+		associate[associateRole][id] = true;
+}
+
+function setEditProjectContent() {
+	let element;
+	let peopleListElement;
+	let imgUrl;
+
+	element = document.getElementById('summary-input-content');
+	element.value = projectData['summary'];
+
+	element = document.getElementById('description-input');
+	element.value = projectData['description'];
+
+	element = document.getElementById('create-priority');
+	element.value = projectData['priority'];
+
+	if(projectData['deadline'] != null) {
+		element = document.getElementById('deadline-input');
+		element.textContent = projectData['deadline'];
+	}
+
+	// set owner
+	peopleListElement = document.getElementById('owner-people-list');
+	for(let i = 0; i < projectData['owner'].length; i++) {
+		imgUrl = null;
+		if(projectData['owner'][i]['image_filename'] != null) {
+			imgUrl = `https://d2o8k69neolkqv.cloudfront.net/project-note/user_img/${projectData['owner'][i]['image_filename']}`;
+		}
+		addClickEffect(imgUrl, projectData['owner'][i]['name'], projectData['owner'][i]['id'], peopleListElement, 'owner');
+	}
+
+	// set reviewer
+	peopleListElement = document.getElementById('reviewer-people-list');
+	for(let i = 0; i < projectData['reviewer'].length; i++) {
+		imgUrl = null;
+		if(projectData['reviewer'][i]['image_filename'] != null) {
+			imgUrl = `https://d2o8k69neolkqv.cloudfront.net/project-note/user_img/${projectData['reviewer'][i]['image_filename']}`;
+		}
+		addClickEffect(imgUrl, projectData['reviewer'][i]['name'], projectData['reviewer'][i]['id'], peopleListElement, 'reviewer');
+	}
+
+	// set team
+	peopleListElement = document.getElementById('team-list');
+	for(let i = 0; i < projectData['team'].length; i++) {
+		addClickEffect(null, projectData['team'][i]['name'], projectData['team'][i]['id'], peopleListElement, 'team');
+	}
+}
+
+function setPeopleForEditProject(imgUrl, name, peopleListElement) {
+	let element;
+	let elementContainer;
+
+	elementContainer = document.createElement('div');
+	elementContainer.className = 'project-people-container';
+	peopleListElement.appendChild(elementContainer);
+
+	element = document.createElement('div');
+	element.className = 'people-img';
+	if(imgUrl != null) {
+		element.style.backgroundImage = `url(${imgUrl})`;
+	}
+	elementContainer.appendChild(element);
+
+	element = document.createElement('div');
+	element.className = 'people-text';
+	element.textContent = name;
+	elementContainer.appendChild(element);
 }
 
 function setProjectContent(data) {
@@ -159,6 +273,7 @@ statusSelect.addEventListener('change', changeStatusColor);
 editProjectBtn.addEventListener('click', () => {
 	darkBackgrountElement.classList.remove('unseen');
 	editProjectAreaElement.classList.remove('unseen');
+	setEditProjectContent();
 });
 
 cancelEditBtn.addEventListener('click', () => {
