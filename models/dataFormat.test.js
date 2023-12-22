e16f6752b26d831c51ec7eda64c5df4d141ae849
shaurair <shaurair@gmail.com>
2023-12-23 05:24:34 +0800

    test: add null case

--- models/dataFormat.test.js
+++ models/dataFormat.test.js
@@ -2,4 +2,5 @@ const dataFormat = require('./dataFormat');
 
 test('check date formate transformation', ()=>{
 	expect(dataFormat.setDateFormateSlash('2023-12-01')).toBe('2023/12/01');
+	expect(dataFormat.setDateFormateSlash(null)).toBe(null);
 })
